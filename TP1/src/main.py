6a8e92b62f3ed866ff753d864e165410599f42f1
KOmla <alexlabou03@gmail.com>
2026-01-14 10:08:52 +0100

    DOCS: Adding of the README

--- TP1/src/main.py
+++ TP1/src/main.py
@@ -1,8 +1,8 @@
-from crawler import crawl
+from crawler_ import Crawler
+crawler = Crawler("https://web-scraping.dev/product/12")
+corpus = crawler.crawl()
 
-
-if __name__ == "__main__":
-    seed_url = "https://web-scraping.dev/products"
-    results = crawl(seed_url)
-    for page in results:
-        print(page)
+corpus_file = "./output/corpus.json"
+with open(corpus_file, "w", encoding="utf-8") as f:
+    import json
+    json.dump(corpus, f, ensure_ascii=False, indent=4)  
